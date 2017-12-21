f9852fbcc4a17091e1912694328f7b7c80564c07
kalibani <kalibani.ka@gmail.com>
2017-12-21 22:12:39 +0700

    konfigurasi db, bucket, keyfile, dll

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ require('dotenv').config()
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/signmeupapi', { useMongoClient: true });
+mongoose.connection.openUri(`mongodb://${process.env.USERNAMEDB}:${process.env.PASSWORDDB}@cluster0-shard-00-00-xrrgq.mongodb.net:27017,cluster0-shard-00-01-xrrgq.mongodb.net:27017,cluster0-shard-00-02-xrrgq.mongodb.net:27017/signmeupapi?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`, { useMongoClient: true })
 mongoose.Promise = global.Promise;
+mongoose.connection.once('open', () => {
+  console.log('mongoose connection success');
+}).on('error', (error) => {
+  console.log('connection error');
+})
 
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
